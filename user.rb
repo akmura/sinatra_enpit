a1aea6f57ce3abdb251c784d7fc7bfa8f2ab7d3e
tantan5 <jojo1938043@gmail.com>
2016-09-19 06:43:45 +0000

    URLによるログインが不要となったため削除

--- user.rb
+++ user.rb
@@ -5,22 +5,25 @@ end
 
 post '/login' do
 @name = params['name']
-redirect to ('/user/'+@name)
-end
-
-get '/user/' do
-if(session['name'] != nil)
-# セッション情報がある場合は当該ユーザページにリダイレクト
-@name = session['name']
-redirect to ('/user/'+@name)
-end
-erb :user
-end
-
-get '/user/:name' do |n|
-@name = n
 session['name']=@name
+redirect to ('/user')
+end
+
+get '/user' do
+if(session['name'] == nil)
+# セッション情報がない場合はログインページにリダイレクト
+redirect to ('/login')
+end
+@name = session['name']
 erb :user
 end
+
+#get '/user/:name' do |n|
+#@name = n
+#session['name']=@name
+#erb :user
+#end
+
+
 
 
