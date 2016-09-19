795f12987cab97d53303e9497fe0076fc81046c6
tantan5 <jojo1938043@gmail.com>
2016-09-19 06:37:33 +0000

    ログインページの追加

--- user.rb
+++ user.rb
@@ -1,3 +1,13 @@
+get '/login' do
+session['name']=nil
+erb :login
+end
+
+post '/login' do
+@name = params['name']
+redirect to ('/user/'+@name)
+end
+
 get '/user/' do
 if(session['name'] != nil)
 # セッション情報がある場合は当該ユーザページにリダイレクト
